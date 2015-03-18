d7421db94a479f4ae5dde7c144f61378b59e1bbc
Jai Krishna <jaikrishna@vmware.com>
2015-03-18 23:27:15 +0530

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+##
+## This file defines a pair of functions that implement
+## matrix inversion. Matrix inversion is an expensive
+## operation; the functions in this file provide for
+## caching a once-computed inverse in order to significantly 
+## speed up repeated inversions of the same matrix.
+##
+## usage:
+##       matrix  <- makeCacheMatrix( rbind( c(1, -1/4), c(-1/4, 1) ) )
+##       inverse <- cacheSolve( matrix )
+##
 
-## Write a short comment describing this function
-
+## Returns a wrapper around an invertible square matrix,
+## and its inverse along with accessors and mutators for the 
+## matrix and its inverse. 
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+          x <<- y
+          inverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inv) inverse <<- inv
+        getInverse <- function() inverse     
+        list( set = set
+            , get = get
+            , setInverse = setInverse
+            , getInverse = getInverse )
 }
 
-
-## Write a short comment describing this function
-
+## Returns the inverse of an invertible square matrix.
+## The inverse is computed upon first invocation and 
+## cached; subsequent invocations return the cached 
+## inverse.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse() 
+        if( is.null(inv) ){
+            inv <- solve( x$get() )
+            x$setInverse(inv)
+        }
+        inv
 }
+
+## end
